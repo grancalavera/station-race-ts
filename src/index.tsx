35b98d354ce4c4a117f1c30002939cf3236ac344
Leon Coto <leoncoto@gmail.com>
2018-10-10 14:14:54 +0100

    Extract <Setup />

--- src/index.tsx
+++ src/index.tsx
@@ -454,6 +454,53 @@ function Begin(state: Begin & { onSetupNewGame: () => void }) {
   );
 }
 
+function Setup(
+  state: Setup & {
+    onStart: () => void;
+    onRegisterPlayer: (player: PlayerRegistration) => void;
+  }
+) {
+  const { onStart, onRegisterPlayer } = state;
+  const handleOnChange = (i: number) => (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+    onRegisterPlayer({ i, name: e.target.value });
+  };
+  return (
+    <React.Fragment>
+      <Keyboard onEnter={onStart} />
+      <ul>
+        <li>Add at least {state.minPlayers} players to start the game.</li>
+        <li>You can add up to {state.maxPlayers} players.</li>
+      </ul>
+      {R.range(0, state.maxPlayers).map((_, i) => (
+        <div key={i} className="editor">
+          <input
+            value={state.registeredPlayers[i] ? state.registeredPlayers[i] : ""}
+            onChange={handleOnChange(i)}
+          />{" "}
+        </div>
+      ))}
+
+      {hasEnoughPlayers(state) && (
+        <div className="control-bar">
+          <button
+            className="control control-large"
+            onClick={onStart}
+            tabIndex={-1}
+          >
+            START
+          </button>
+        </div>
+      )}
+      <ul className="small-print">
+        {hasEnoughPlayers(state) && <li>Enter: start game.</li>}
+      </ul>
+    </React.Fragment>
+  );
+}
+
 class StationRace extends React.Component<Configuration, State> {
   constructor(props: Configuration) {
     super(props);
@@ -473,15 +520,11 @@ class StationRace extends React.Component<Configuration, State> {
     const sendNextTurn = () => this.sendInput({ type: "NextTurn" });
     const sendPlayAgain = () => this.sendInput({ type: "PlayAgain" });
     const sendBeginAgain = () => this.sendInput({ type: "BeginAgain" });
-    const sendRegisterPlayer = (i: number) => (
-      e: React.ChangeEvent<HTMLInputElement>
-    ) => {
-      e.preventDefault();
+    const sendRegisterPlayer = (player: PlayerRegistration) =>
       this.sendInput({
-        payload: { i, name: e.target.value },
+        payload: player,
         type: "RegisterPlayer"
       });
-    };
 
     return (
       <React.Fragment>
@@ -498,40 +541,11 @@ class StationRace extends React.Component<Configuration, State> {
         )}
 
         {stateIsSetup(state) && (
-          <React.Fragment>
-            <Keyboard onEnter={sendStart} />
-            <ul>
-              <li>
-                Add at least {state.minPlayers} players to start the game.
-              </li>
-              <li>You can add up to {state.maxPlayers} players.</li>
-            </ul>
-            {R.range(0, state.maxPlayers).map((_, i) => (
-              <div key={i} className="editor">
-                <input
-                  value={
-                    state.registeredPlayers[i] ? state.registeredPlayers[i] : ""
-                  }
-                  onChange={sendRegisterPlayer(i)}
-                />{" "}
-              </div>
-            ))}
-
-            {hasEnoughPlayers(state) && (
-              <div className="control-bar">
-                <button
-                  className="control control-large"
-                  onClick={sendStart}
-                  tabIndex={-1}
-                >
-                  START
-                </button>
-              </div>
-            )}
-            <ul className="small-print">
-              {hasEnoughPlayers(state) && <li>Enter: start game.</li>}
-            </ul>
-          </React.Fragment>
+          <Setup
+            {...state}
+            onStart={sendStart}
+            onRegisterPlayer={sendRegisterPlayer}
+          />
         )}
 
         {stateIsAnyTurn(state) && (
