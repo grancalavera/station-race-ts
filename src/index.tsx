f43a1fc505f350d5e51cfa0dad1f62db12c5af3b
Leon Coto <leoncoto@gmail.com>
2018-10-10 14:02:16 +0100

    Extract <Begin />

--- src/index.tsx
+++ src/index.tsx
@@ -423,6 +423,37 @@ class Keyboard extends React.Component<KeyboardProps> {
   }
 }
 
+function Begin(state: Begin & { onSetupNewGame: () => void }) {
+  const { onSetupNewGame } = state;
+  return (
+    <React.Fragment>
+      <Keyboard onEnter={onSetupNewGame} />
+      <ul>
+        <li>
+          You're in a train running from station {state.firstStation} to station{" "}
+          {state.lastStation}.
+        </li>
+        <li>
+          There is a secret station and you need to get off the train there.
+        </li>
+        <li>Be the first one to guess the secret station and win the game!</li>
+      </ul>
+      <div className="control-bar">
+        <button
+          className="control control-large"
+          onClick={onSetupNewGame}
+          tabIndex={-1}
+        >
+          BEGIN
+        </button>
+      </div>
+      <ul className="small-print">
+        <li>Enter: begin the game.</li>
+      </ul>
+    </React.Fragment>
+  );
+}
+
 class StationRace extends React.Component<Configuration, State> {
   constructor(props: Configuration) {
     super(props);
@@ -463,34 +494,7 @@ class StationRace extends React.Component<Configuration, State> {
         )}
 
         {stateIsBegin(state) && (
-          <React.Fragment>
-            <Keyboard onEnter={sendSetupNewGame} />
-            <ul>
-              <li>
-                You're in a train running from station {state.firstStation} to
-                station {state.lastStation}.
-              </li>
-              <li>
-                There is a secret station and you need to get off the train
-                there.
-              </li>
-              <li>
-                Be the first one to guess the secret station and win the game!
-              </li>
-            </ul>
-            <div className="control-bar">
-              <button
-                className="control control-large"
-                onClick={sendSetupNewGame}
-                tabIndex={-1}
-              >
-                BEGIN
-              </button>
-            </div>
-            <ul className="small-print">
-              <li>Enter: begin the game.</li>
-            </ul>
-          </React.Fragment>
+          <Begin {...state} onSetupNewGame={sendSetupNewGame} />
         )}
 
         {stateIsSetup(state) && (
